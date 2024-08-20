8e933425c0ea55e7833e16c06142f846dad40d77
ap5252 <131720163+ap5252@users.noreply.github.com>
2024-08-20 19:01:04 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    const sections = document.querySelectorAll('section');
-    sections.forEach(section => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top < window.innerHeight) {
-            section.classList.add('visible');
-        }
-    });
-});
